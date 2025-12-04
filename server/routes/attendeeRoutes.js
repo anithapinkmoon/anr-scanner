b404c3ef3ef8328848b2726da050e44828da1d23
anithapinkmoon <anitha@pinkmoontech.com>
2025-12-04 19:14:28 +0530

    Update attendeeRoutes.js

--- server/routes/attendeeRoutes.js
+++ server/routes/attendeeRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { registerAttendee, verifyQRCode } from '../controllers/attendeeController.js';
+import { registerAttendee, verifyQRCode, getRegisteredAttendees } from '../controllers/attendeeController.js';
 
 const router = express.Router();
 
 router.post('/register', registerAttendee);
 router.get('/verify', verifyQRCode);
+router.get('/attendees', getRegisteredAttendees);
 
 export default router;
 
